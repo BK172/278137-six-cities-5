aa6a47ba7a24412862bdd00489a0e4fb92caca60
BK172 <bk-172-v@yandex.ru>
2020-10-12 13:30:49 +0300

    Добавлены моковые данные

---
+++ src/mocks/offers.js
@@ -0,0 +1,148 @@
+export default [
+	{
+		id: 0,
+		image: `img/apartment-01.jpg`,
+		premium: true,
+		cost: 120,
+		title: `Beautiful & luxurious studio at great location`,
+		type: `apartment`,
+		rating: 4.4,
+		photos: [
+			`img/room.jpg`,
+			`img/apartment-02.jpg`,
+			`img/apartment-03.jpg`,
+			`img/apartment-01.jpg`,
+			`img/apartment-02.jpg`,
+			`img/studio-01.jpg`
+		],
+		description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century`,
+		bedrooms: `3 Bedrooms`,
+		guests: `Max 4 adults`,
+		facilities: [
+			`Wi-Fi`,
+			`Washing machine`,
+			`Towels`,
+			`Heating`,
+			`Coffee machine`,
+			`Baby seat`,
+			`Kitchen`,
+			`Dishwasher`,
+			`Cabel TV`,
+			`Fridge`,
+		],
+		owner: {
+			avatar: `img/avatar-angelina.jpg`,
+			name: `Angelina`,
+			super: true
+		}
+	},
+	{
+		id: 1,
+		image: `img/room.jpg`,
+		premium: false,
+		cost: 80,
+		title: `Wood and stone place`,
+		type: `room`,
+		rating: 4.2,
+		photos: [
+			`img/room.jpg`,
+			`img/apartment-02.jpg`,
+			`img/apartment-03.jpg`,
+			`img/apartment-01.jpg`,
+			`img/studio-01.jpg`
+		],
+		description: `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`,
+		bedrooms: `2 Bedrooms`,
+		guests: `Max 3 adults`,
+		facilities: [
+			`Wi-Fi`,
+			`Washing machine`,
+			`Towels`,
+			`Heating`,
+			`Coffee machine`,
+			`Baby seat`,
+			`Kitchen`,
+			`Dishwasher`,
+			`Cabel TV`,
+			`Fridge`,
+		],
+		owner: {
+			avatar: `img/avatar-angelina.jpg`,
+			name: `Angelina`,
+			super: true
+		}
+	},
+	{
+		id: 2,
+		image: `img/apartment-02.jpg`,
+		premium: false,
+		cost: 132,
+		title: `Beautiful & luxurious studio at great location`,
+		type: `apartment`,
+		rating: 3.9,
+		photos: [
+			`img/room.jpg`,
+			`img/apartment-02.jpg`,
+			`img/apartment-03.jpg`,
+			`img/apartment-01.jpg`,
+			`img/apartment-02.jpg`,
+			`img/studio-01.jpg`
+		],
+		description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century`,
+		bedrooms: `2 Bedrooms`,
+		guests: `Max 3 adults`,
+		facilities: [
+			`Wi-Fi`,
+			`Washing machine`,
+			`Towels`,
+			`Heating`,
+			`Coffee machine`,
+			`Baby seat`,
+			`Kitchen`,
+			`Dishwasher`,
+			`Cabel TV`,
+			`Fridge`,
+		],
+		owner: {
+			avatar: `img/avatar-angelina.jpg`,
+			name: `Angelina`,
+			super: true
+		}
+	},
+	{
+		id: 3,
+		image: `img/apartment-03.jpg`,
+		premium: true,
+		cost: 180,
+		title: `Nice, cozy, warm big bed apartment`,
+		type: `Apartment`,
+		rating: 4.5,
+		photos: [
+			`img/room.jpg`,
+			`img/apartment-02.jpg`,
+			`img/apartment-03.jpg`,
+			`img/apartment-01.jpg`,
+			`img/studio-01.jpg`
+		],
+		description: `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`,
+		bedrooms: `2 Bedrooms`,
+		guests: `Max 3 adults`,
+		facilities: [
+			`Wi-Fi`,
+			`Washing machine`,
+			`Towels`,
+			`Heating`,
+			`Coffee machine`,
+			`Baby seat`,
+			`Kitchen`,
+			`Dishwasher`,
+			`Cabel TV`,
+			`Fridge`,
+		],
+		owner: {
+			avatar: `img/avatar-angelina.jpg`,
+			name: `Angelina`,
+			super: true
+		}
+	}
+];
