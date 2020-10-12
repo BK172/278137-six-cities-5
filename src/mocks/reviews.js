90c161777b35c1e52872747fd563821a795a8ff9
BK172 <bk-172-v@yandex.ru>
2020-10-12 13:39:36 +0300

    Поправки моковых данных

--- src/mocks/reviews.js
+++ src/mocks/reviews.js
@@ -1,18 +1,18 @@
 export default [
-	{
-		id: 0,
-		avatar: `img/avatar-max.jpg`,
-		name: `Max`,
-		rating: 4,
-		comment: `Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?`,
-		date: `2019-04-24`
-	},
-	{
-		id: 1,
-		avatar: `img/avatar-angelina.jpg`,
-		name: `Angelina`,
-		rating: 3,
-		comment: `Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?`,
-		date: `2019-05-20`
-	},
-]
+  {
+    id: 0,
+    avatar: `img/avatar-max.jpg`,
+    name: `Max`,
+    rating: 4,
+    comment: `Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?`,
+    date: `2019-04-24`
+  },
+  {
+    id: 1,
+    avatar: `img/avatar-angelina.jpg`,
+    name: `Angelina`,
+    rating: 3,
+    comment: `Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?`,
+    date: `2019-05-20`
+  }
+];
