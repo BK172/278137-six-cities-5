ad963f07286c6d3e82585dfca0fc52949007bcd0
BK172 <bk-172-v@yandex.ru>
2020-10-12 14:09:08 +0300

    Завел новые компоненты и поправил старые

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import App from "./components/app/app";
 import offers from "./mocks/offers";
 import reviews from "./mocks/reviews";
 
-const OFFERS_COUNT = 5;
-
 ReactDOM.render(
-    <App    
-        offers = {offers}
-        reviews = {reviews} 
+    <App
+      offers={offers}
+      reviews={reviews}
     />,
     document.querySelector(`#root`)
 );
