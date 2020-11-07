de441fbe5da15b295d6429ee2b6134175666b4f1
BK172 <bk-172-v@yandex.ru>
2020-11-07 17:36:59 +0300

    Добавил компонент main-empty

---
+++ src/components/main-empty/main-empty.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {citiesPropTypes} from "../../app-prop-types";
+
+const MainEmpty = ({activeCity}) => {
+  return (
+    <React.Fragment>
+      <section className="cities__no-places">
+        <div className="cities__status-wrapper tabs__content">
+          <b className="cities__status">No places to stay available</b>
+          <p className="cities__status-description">We could not find any property available at the moment
+            in {activeCity.name}</p>
+        </div>
+      </section>
+      <div className="cities__right-section"/>
+    </React.Fragment>
+  );
+};
+
+MainEmpty.propTypes = {
+  activeCity: citiesPropTypes,
+};
+
+export default MainEmpty;
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import Header from "../header/header";
+import MainEmpty from "../main-empty/main-empty";
 import OfferList from "../offer-list/offer-list";
 import CitiesList from "../cities-list/cities-list";
 import SortingOptions from "../sorting-options/sorting-options";
@@ -9,12 +10,15 @@ import Map from "../map/map";
 import {offerPropTypes, citiesPropTypes} from "../../app-prop-types";
 
 const Main = ({offers, activeCity}) => {
+  const getPageClass = () => offers.length === 0 ? `page__main--index-empty` : ``;
+  const getContainerClass = () => offers.length === 0 ? `cities__places-container--empty` : ``;
+
   return (
     <React.Fragment>
       <div style={{display: `none`}}>
         <svg xmlns="http://www.w3.org/2000/svg"><symbol id="icon-arrow-select" viewBox="0 0 7 4"><path fillRule="evenodd" clipRule="evenodd" d="M0 0l3.5 2.813L7 0v1.084L3.5 4 0 1.084V0z" /></symbol><symbol id="icon-bookmark" viewBox="0 0 17 18"><path d="M3.993 2.185l.017-.092V2c0-.554.449-1 .99-1h10c.522 0 .957.41.997.923l-2.736 14.59-4.814-2.407-.39-.195-.408.153L1.31 16.44 3.993 2.185z" /></symbol><symbol id="icon-star" viewBox="0 0 13 12"><path fillRule="evenodd" clipRule="evenodd" d="M6.5 9.644L10.517 12 9.451 7.56 13 4.573l-4.674-.386L6.5 0 4.673 4.187 0 4.573 3.549 7.56 2.483 12 6.5 9.644z" /></symbol></svg>
       </div>
-      <div className="page page--gray page--main">
+      <div className={`page page--gray page--main ${getPageClass()}`}>
         <Header />
         <main className="page__main page__main--index">
           <h1 className="visually-hidden">Cities</h1>
@@ -24,18 +28,24 @@ const Main = ({offers, activeCity}) => {
             </section>
           </div>
           <div className="cities">
-            <div className="cities__places-container container">
-              <section className="cities__places places">
-                <h2 className="visually-hidden">Places</h2>
-                <b className="places__found">{offers.length} places to stay in {activeCity.name}</b>
-                <SortingOptions />
-                <div className="cities__places-list places__list tabs__content">
-                  <OfferList offers={offers} />
-                </div>
-              </section>
-              <div className="cities__right-section">
-                <Map mapType={`cities`} offers={offers} />
-              </div>
+            <div className={`cities__places-container container ${getContainerClass()}`}>
+              {offers.length > 0 ? (
+                <React.Fragment>
+                  <section className="cities__places places">
+                    <h2 className="visually-hidden">Places</h2>
+                    <b className="places__found">{offers.length} places to stay in {activeCity.name}</b>
+                    <SortingOptions />
+                    <div className="cities__places-list places__list tabs__content">
+                      <OfferList offers={offers} />
+                    </div>
+                  </section>
+                  <div className="cities__right-section">
+                    <Map mapType={`cities`} offers={offers} />
+                  </div>
+                </React.Fragment>
+              ) : (
+                <MainEmpty activeCity={activeCity} />
+              )}
             </div>
           </div>
         </main>
