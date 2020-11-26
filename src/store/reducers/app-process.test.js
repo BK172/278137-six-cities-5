ecdaa274b03e1d9130412031a8a444b9a9faddb8
BK172 <bk-172-v@yandex.ru>
2020-11-26 16:22:06 +0300

    Добавил тесты для reducers и actions

---
+++ src/store/reducers/app-process.test.js
@@ -0,0 +1,61 @@
+import {appProcess} from "./app-process";
+import {ActionType} from "../action";
+import {INITIAL_SORTING_TYPE} from "../../constants";
+import {mockCity, mockOffer} from "../../utils";
+
+jest.mock(`./root-reducer`);
+
+describe(`Should reducer appProcess works correctly`, () => {
+  it(`Should reducer appProcess return initial state`, () => {
+    expect(appProcess(undefined, {})).toEqual({
+      activeOffer: null,
+      activeCity: {},
+      sortingType: INITIAL_SORTING_TYPE,
+      isLoadingFlag: false,
+    });
+  });
+
+  it(`Should reducer appProcess update sortingType`, () => {
+    expect(appProcess({
+      sortingType: INITIAL_SORTING_TYPE,
+    }, {
+      type: ActionType.CHANGE_SORTING_TYPE,
+      payload: `top-rated`
+    })).toEqual({
+      sortingType: `top-rated`
+    });
+  });
+
+  it(`Should reducer appProcess update activeCity`, () => {
+    expect(appProcess({
+      activeCity: {},
+    }, {
+      type: ActionType.SET_ACTIVE_CITY,
+      payload: mockCity
+    })).toEqual({
+      activeCity: mockCity
+    });
+  });
+
+  it(`Should reducer appProcess update activeOffer`, () => {
+    expect(appProcess({
+      activeOffer: null,
+    }, {
+      type: ActionType.SET_ACTIVE_OFFER,
+      payload: mockOffer
+    })).toEqual({
+      activeOffer: mockOffer
+    });
+  });
+
+  it(`Should reducer appProcess update isLoadingFlag`, () => {
+    expect(appProcess({
+      isLoadingFlag: false,
+    }, {
+      type: ActionType.IS_LOADING,
+      payload: true
+    })).toEqual({
+      isLoadingFlag: true
+    });
+  });
+});
---
+++ src/store/reducers/user.test.js
@@ -0,0 +1,24 @@
+import {user} from "./user";
+import {ActionType} from "../action";
+import {AuthStatus} from "../../constants";
+
+jest.mock(`./root-reducer`);
+
+describe(`Should reducer user works correctly`, () => {
+  it(`Should reducer user return initial state`, () => {
+    expect(user(undefined, {})).toEqual({
+      authStatus: AuthStatus.NO_AUTH,
+    });
+  });
+
+  it(`Should reducer user should update authStatus to AuthStatus.AUTH`, () => {
+    expect(user({
+      authStatus: AuthStatus.NO_AUTH,
+    }, {
+      type: ActionType.REQUIRE_AUTHORIZATION,
+      payload: AuthStatus.AUTH
+    })).toEqual({
+      authStatus: AuthStatus.AUTH
+    });
+  });
+});
